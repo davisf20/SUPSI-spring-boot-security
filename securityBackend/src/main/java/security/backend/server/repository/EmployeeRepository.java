5077f0fe22d1c75cb669cd68fda99bdca6a29ab9
Davis Fusco <davis.fusco.20@gmail.com>
2023-01-29 23:19:37 +0100

    feat: PreparedStatment

--- securityBackend/src/main/java/security/backend/server/repository/EmployeeRepository.java
+++ securityBackend/src/main/java/security/backend/server/repository/EmployeeRepository.java
@@ -1,10 +1,12 @@
 package security.backend.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import security.backend.server.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Query(value = "SELECT * FROM Employee WHERE username = ?1", nativeQuery = true)
     Employee findByUsername(String username);
 }
